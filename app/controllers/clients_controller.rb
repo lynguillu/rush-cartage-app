d056f16c9eedfae72019ae203ecbc9ce23a17e4e
Lynda Guillu <lynda_guillu@att.net>
2017-04-30 15:28:05 -0500

    AddLoginPages

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,28 @@
 class ClientsController < ApplicationController
   def index
     @clients = Client.all
-    render 'index.html.erb'
+    render "index.html.erb"
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client = Client.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if client.save
+      session[:client_id] = client.id
+      flash[:success] = 'Successfully created client!'
+      redirect_to '/'
+    else
+      flash[:warning] = 'Invalid email or password!'
+      redirect_to '/clients_signup'
+    end
   end
 
   def show
