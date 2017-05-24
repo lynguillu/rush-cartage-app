791827fb5cfb45775cd665580b6a46d185863407
Lynda Guillu <lynda_guillu@att.net>
2017-05-23 19:56:00 -0500

    Clean API On Admin Page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,14 @@ class AdminsController < ApplicationController
   def index
     @pods = Pod.all
     @admins = Admin.all
+      @data = Unirest.get("http://api.eia.gov/series/?api_key=bea6202b7a787c0b4d1daf15108ae4f7&series_id=PET.EMD_EPD2D_PTE_R20_DPG.W").body
+    @series_data = @data["series"][0]["data"]
+    @geography = @data["series"][0]["geography"]
+    @updated = @data["series"][0]["updated"]
+    @name = @data["series"][0]["name"]
+    @units = @data["series"][0]["units"]
     render "index.html.erb"
+
   end
 
   def new
