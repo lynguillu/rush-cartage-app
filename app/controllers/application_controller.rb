f50bd231104f3470182580cc968fecee9df5b77f
Lynda Guillu <lynda_guillu@att.net>
2017-05-24 18:57:00 -0500

    Personalize Index Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+
 
   def current_admin
     @current_admin ||= Admin.find_by(id: session[:admin_id]) if session[:admin_id]
